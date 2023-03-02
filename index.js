8df55758f809e477aa0806f93baa9f8e46e0151e
Cherise Foster <cherise.foster@outlook.com>
2023-03-02 14:56:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(wins){
+    for (const record of wins) {
+        if (record.result === "W") {
+            return record.year;
+        }
+    }
+};
